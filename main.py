1f044ccf81447741055179c295c22745de2742ce
Victor <Vilala593@gmail.com>
2022-09-14 11:01:49 +0300

    v 0.0.2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import faker
 
 faker_obj = faker.Faker()
 
-gender = 'M'
+gender = 'M' 
 male_profile = faker_obj.simple_profile(sex=gender)
 phone = faker_obj.phone_number()
 print("Male profile generated is as follows:\n", male_profile , phone)
